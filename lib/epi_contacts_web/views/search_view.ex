05f8836a5b97838c8637aff79372abc9aa41c3b5
Pat Maddox <pat@ratiopbc.com>
2021-12-06 15:06:50 -0800

    Remove unused function

--- lib/epi_contacts_web/views/search_view.ex
+++ lib/epi_contacts_web/views/search_view.ex
@@ -5,14 +5,6 @@ defmodule EpiContactsWeb.SearchView do
   def number_of_search_results([_one_result]), do: "1 result"
   def number_of_search_results(search_results), do: "#{length(search_results)} results"
 
-  def search_criteria(first_name, last_name, street, city, state, zip_code, phone) do
-    full_name = [first_name, last_name] |> Enum.join(" ")
-
-    [full_name, street, city, state, zip_code, phone]
-    |> Enum.reject(&(!&1))
-    |> Enum.map_join(", ", &"\"#{&1}\"")
-  end
-
   def full_name(%{first_name: first_name, middle_name: middle_name, last_name: last_name}) do
     [first_name, middle_name, last_name]
     |> Enum.reject(&(!&1))
