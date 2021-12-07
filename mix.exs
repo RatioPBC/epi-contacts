ea46b8dbdc1303377a860f4f431b405e11cc2d7b
Pat Maddox <pat@ratiopbc.com>
2021-12-07 09:59:44 -0800

    Don't fail build because of coverage

--- mix.exs
+++ mix.exs
@@ -20,7 +20,10 @@ defmodule EpiContacts.MixProject do
           ]
         ]
       ],
-      xref: [exclude: IEx]
+      xref: [exclude: IEx],
+      test_coverage: [
+        summary: [ threshold: 0 ]
+      ]
     ]
   end
 
