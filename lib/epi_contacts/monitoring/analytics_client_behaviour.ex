bf9c29c0aebd4869f13f72d1205f3fc9b95ecc9c
Jesse Cooke <jesse@relativepath.io>
2021-12-21 15:01:20 -0800

    Fix typespec

--- lib/epi_contacts/monitoring/analytics_client_behaviour.ex
+++ lib/epi_contacts/monitoring/analytics_client_behaviour.ex
@@ -2,6 +2,6 @@ defmodule EpiContacts.Monitoring.AnalyticsClientBehaviour do
   @moduledoc """
   Provides a mockable interface around metrics reporting
   """
-  @callback capture(String.t() | atom(), Map.t(), String.t()) :: {:ok | :error, any()}
+  @callback capture(String.t() | atom(), map(), String.t()) :: {:ok | :error, any()}
   @callback configuration_present?() :: boolean()
 end
